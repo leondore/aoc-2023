3dfc4889570e0dd5d828f83507c7b3b3aed2ab71
Leon Dore <leon.dore@gmail.com>
2024-02-28 14:55:37 -0400

    Day 1 - Part 1

--- puzzles/01/main.go
+++ puzzles/01/main.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/leondore/aoc-2023/utils"
 )
 
 func main() {
-	inputData := utils.ReadFile("puzzles/01/test.txt")
+	inputData := utils.ReadFile("puzzles/01/input.txt")
 	lines := strings.Split(inputData, "\n")
 
+	re := regexp.MustCompile(`[^0-9]`)
+
+	var result int64
+
 	for _, line := range lines {
-		fmt.Println(line)
+		if len(line) == 0 {
+			continue
+		}
+
+		digits := re.ReplaceAllString(line, "")
+
+		calibrationValueS := string(digits[0]) + string(digits[len(digits)-1])
+		calibrationValue, err := strconv.ParseInt(calibrationValueS, 10, 64)
+		utils.CheckError(err)
+
+		result += calibrationValue
 	}
+
+	fmt.Println(result)
 }
