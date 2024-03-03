17f31a3f9160ff59a559cb116fb5b320501e041b
Leon Dore <leon.dore@gmail.com>
2024-03-02 21:03:00 -0400

    Day 1 - Part 2 (don't love this)

--- puzzles/01/main.go
+++ puzzles/01/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"regexp"
+	"sort"
 	"strconv"
 	"strings"
 
@@ -13,18 +13,37 @@ func main() {
 	inputData := utils.ReadFile("puzzles/01/input.txt")
 	lines := strings.Split(inputData, "\n")
 
-	re := regexp.MustCompile(`[^0-9]`)
+	numbers := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
 
 	var result int64
 
+	type numidx struct {
+		Number string
+		Index  int
+	}
+
 	for _, line := range lines {
-		if len(line) == 0 {
+		var numsFound []numidx
+		for _, number := range numbers {
+			idxf := strings.Index(line, number)
+			if idxf != -1 {
+				numsFound = append(numsFound, numidx{Number: number, Index: idxf})
+			}
+			idxl := strings.LastIndex(line, number)
+			if idxl != -1 {
+				numsFound = append(numsFound, numidx{Number: number, Index: idxl})
+			}
+		}
+
+		if len(numsFound) == 0 {
 			continue
 		}
 
-		digits := re.ReplaceAllString(line, "")
+		sort.Slice(numsFound, func(i, j int) bool {
+			return numsFound[i].Index < numsFound[j].Index
+		})
 
-		calibrationValueS := string(digits[0]) + string(digits[len(digits)-1])
+		calibrationValueS := convertToDigit(numsFound[0].Number) + convertToDigit(numsFound[len(numsFound)-1].Number)
 		calibrationValue, err := strconv.ParseInt(calibrationValueS, 10, 64)
 		utils.CheckError(err)
 
@@ -32,4 +51,23 @@ func main() {
 	}
 
 	fmt.Println(result)
+}
+
+func convertToDigit(number string) string {
+	numbersMap := make(map[string]string)
+	numbersMap["one"] = "1"
+	numbersMap["two"] = "2"
+	numbersMap["three"] = "3"
+	numbersMap["four"] = "4"
+	numbersMap["five"] = "5"
+	numbersMap["six"] = "6"
+	numbersMap["seven"] = "7"
+	numbersMap["eight"] = "8"
+	numbersMap["nine"] = "9"
+
+	if _, ok := numbersMap[number]; !ok {
+		return number
+	}
+
+	return numbersMap[number]
 }
